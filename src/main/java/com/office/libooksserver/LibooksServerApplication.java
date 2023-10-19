26052db48aee2d97a234bb1510a890d0391053c2
beanbeeean <beanbeeean@naver.com>
2023-10-19 12:15:41 +0900

    Initial commit

---
+++ src/main/java/com/office/libooksserver/LibooksServerApplication.java
@@ -0,0 +1,13 @@
+package com.office.libooksserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibooksServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibooksServerApplication.class, args);
+    }
+
+}
