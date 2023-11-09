017d047eeaa5edb0a4752237970ffc70e13060ab
siyeong1013 <tldud4346@gmail.com>
2023-11-09 09:01:35 +0900

    [TF-64] Community Image Upload

--- src/main/java/com/office/libooksserver/user/controller/CommunityController.java
+++ src/main/java/com/office/libooksserver/user/controller/CommunityController.java
@@ -37,19 +37,22 @@ public class CommunityController {
         return communityService.getCommunity(keyword,category,searchOption);
     }
 
-    @GetMapping("/write")
+    @PostMapping("/write")
     @ResponseBody
-    public Object writeCommunity(@RequestParam(name = "selection", required = false) int category,
-                                 @RequestParam(name = "title", required = false) String title,
-                                 @RequestParam(name = "content", required = false) String content,
-                                 @RequestParam(name = "u_email", required = false) String u_email
+    public Object writeCommunity(@RequestBody Map<String, String> map
+//            @RequestParam(name = "selection", required = false) int category,
+//                                 @RequestParam(name = "title", required = false) String title,
+//                                 @RequestParam(name = "content", required = false) String content,
+//                                 @RequestParam(name = "u_email", required = false) String u_email
     ) {
         log.info("[CommunityController] writeCommunity()");
-        log.info("category" + category);
-        log.info("title" + title);
-        log.info("content" + content);
+        log.info("category" + map.get("selection"));
+        log.info("title" + map.get("title"));
+        log.info("content" + map.get("content"));
+//        int result = 0;
 
-        int result = communityService.writeCommunity(category, title, content, u_email);
+
+        int result = communityService.writeCommunity(Integer.parseInt(map.get("selection")), map.get("title"), map.get("content"), map.get("u_email"));
 
         return result;
     }
@@ -66,19 +69,20 @@ public class CommunityController {
         return communityDto;
     }
 
-    @GetMapping("/community_modify/{id}")
+    @PostMapping("/community_modify/{id}")
     @ResponseBody
-    public Object modifyCommunity(@PathVariable int id,
-                                  @RequestParam(name = "selection", required = false) int category,
-                                 @RequestParam(name = "title", required = false) String title,
-                                 @RequestParam(name = "content", required = false) String content
+    public Object modifyCommunity(@PathVariable int id, @RequestBody Map<String, String> map
+//            @PathVariable int id,
+//                                  @RequestParam(name = "selection", required = false) int category,
+//                                 @RequestParam(name = "title", required = false) String title,
+//                                 @RequestParam(name = "content", required = false) String content
     ) {
         log.info("[CommunityController] modifyCommunity()");
-        log.info("category" + category);
-        log.info("title" + title);
-        log.info("content" + content);
+        log.info("category" + map.get("selection"));
+        log.info("title" + map.get("title"));
+        log.info("content" + map.get("content"));
 
-        int result = communityService.modifyCommunity(id, category, title, content);
+        int result = communityService.modifyCommunity(id, Integer.parseInt(map.get("selection")), map.get("title"), map.get("content"));
 
         return result;
     }
@@ -90,58 +94,6 @@ public class CommunityController {
         int result = communityService.deleteCommunity(id);
 
         return result;
-    }
-
-    // 이미지 불러오기
-    @GetMapping("/getImage/{fileId}/{fileType}")
-    public ResponseEntity<byte[]> getImageFile(@PathVariable String fileId, @PathVariable String fileType) {
-
-        try {
-            FileInputStream fis = new FileInputStream(UPLOAD_PATH + "\\" + fileId + "." + fileType);
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-            byte buffer[] = new byte[1024];
-            int length = 0;
-
-            while((length = fis.read(buffer)) != -1) {
-                baos.write(buffer, 0, length);
-            }
-
-            return new ResponseEntity<byte[]>(baos.toByteArray(), HttpStatus.OK);
-
-        } catch(IOException e) {
-            return new ResponseEntity<byte[]>(new byte[] {}, HttpStatus.CONFLICT);
-        }
-    }
-
-    // 이미지 업로드
-    @PostMapping("/uploadImage")
-    public ResponseEntity<Object> uploadImage(MultipartFile multipartFiles[]) {
-        try {
-            MultipartFile file = multipartFiles[0];
-
-            String fileId = (new Date().getTime()) + "" + (new Random().ints(1000, 9999).findAny().getAsInt()); // 현재 날짜와 랜덤 정수값으로 새로운 파일명 만들기
-            String originName = file.getOriginalFilename(); // ex) 파일.jpg
-            String fileExtension = originName.substring(originName.lastIndexOf(".") + 1); // ex) jpg
-            originName = originName.substring(0, originName.lastIndexOf(".")); // ex) 파일
-            long fileSize = file.getSize(); // 파일 사이즈
-
-            File fileSave = new File(UPLOAD_PATH, fileId + "." + fileExtension); // ex) fileId.jpg
-            if(!fileSave.exists()) { // 폴더가 없을 경우 폴더 만들기
-                fileSave.mkdirs();
-            }
-
-            file.transferTo(fileSave); // fileSave의 형태로 파일 저장
-
-            System.out.println("fileId= " + fileId);
-            System.out.println("originName= " + originName);
-            System.out.println("fileExtension= " + fileExtension);
-            System.out.println("fileSize= " + fileSize);
-
-            return new ResponseEntity<Object>("http://localhost:3000/getImage/" + fileId + "/" + fileExtension, HttpStatus.OK);
-        } catch(IOException e) {
-            return new ResponseEntity<Object>(null, HttpStatus.CONFLICT);
-        }
     }
 
     @PostMapping("/write_comment")
@@ -171,4 +123,5 @@ public class CommunityController {
         Map<String, Object> map = communityService.getComments(c_no);
         return map;
     }
+
 }
