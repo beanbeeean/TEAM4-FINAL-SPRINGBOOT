527acafa98f831c19cbc74f0b5d9428b770df03d
beanbeeean <beanbeeean@naver.com>
2023-11-16 16:06:37 +0900

    fixing

--- src/main/java/com/office/libooksserver/HomeController.java
+++ src/main/java/com/office/libooksserver/HomeController.java
@@ -3,11 +3,12 @@ package com.office.libooksserver;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@Controller
+@RestController
 public class HomeController {
 
     @GetMapping("/")
